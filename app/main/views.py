a6874db509cfb1a72b78184bf036c8b1ebdd863c
becky nayere <rebecca.nayere@gmail.com>
2020-09-24 17:48:18 +0300

    install pip flask

--- app/main/views.py
+++ app/main/views.py
@@ -1,2 +1,8 @@
 from flask import render_template, request, redirect, url_for, abort
 from . import main
+from .forms import 
+from ..models import Forms
+from flask_login import login_required, current_user
+from .. import db, photos
+from flask_login import login_required, current_user
+import markdown2 
--- app/models.py
+++ app/models.py
@@ -8,20 +8,61 @@ from datetime import datetime
 
 
 
-class Pizza():
+class Pizza:
+    '''
+    Pizza ito define Pizza objects
+    '''
+
     __tablename__='pizza'
+     
+    id=db.Column(db.Integer,primary_key=True)
+    size=db.Column(db.String)
+    flavor=db.Column(db.String)
+    toppings=db.Column(db.String)
+    price=db.Column(db.Integer)
+
+    all_pizza = []
+
+
+    def __init__(self, id, toppings,flavor, price):
+
+        self.id = id
+        self.size= size
+        self.flavor= flavor
+        self.toppings=toppings
+        self.price= price
+
+    def save_pizza(self):
+        Pizza.all_pizza.append(self)
+        db.session.add(self)
+        db.session.commit()
+         
+
+    @classsmethod
+    def get_pizza(cls,pizza_id):
+        """
+        Returns a pizza depending on the id
+        """
+        pizza_size=Pizza.query.filter_by(id=pizza_id).all()
+        return pizza_size
+
+class Flavor(db.model):
+    __tablename__="flavor"
+
+    id=db.Column(db.Integer,primary_key=True)
+    pizza_id=db.Column(db.Integer)
+    pizza_size=db.Column(db.String)
+    pizza_toppings=db.Column(db.String)
+    pizza_price=db.relationship('Pizza',backref='pizza',lazy="dynamic")
     
-    id = db.Column(string (255)),primary_key = True)
-    size=db.Column()
-
-
-
-class Flavor():
-     __tablename__ ='flavor'    
     
 
-class Size():
-    __tablename__ = 'size'
+    def save_flavor (self):
+        db.session.add(self)
+        db.session.commit()
 
-class Toppings():
-    __tablename__ = 'toppings'
+
+    @classsmethod
+    def get_flavor(cls,id) :
+        flavor = Flavor.query.filter_by(flavor_id=id).all()
+        return flavor
